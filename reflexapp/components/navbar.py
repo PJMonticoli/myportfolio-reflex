cf49bd1af988fa9d5d3ee47c1b8848f36cbe3dd0
PJMonticoli <pablo_monticoli@hotmail.com>
2024-05-29 10:47:24 -0300

    Hago que el navbar redireccione a mi index

--- reflexapp/components/navbar.py
+++ reflexapp/components/navbar.py
@@ -1,16 +1,19 @@
 import reflex as rx
 import reflexapp.styles.styles as Styles
+import reflexapp.routes.routes as routes
 from reflexapp.styles.styles import Size as Size
 from reflexapp.styles.colors import TextColor as TextColor
 from reflexapp.styles.colors import Colors as Colors
 
 def navbar() -> rx.Component:
     return rx.hstack(
-        rx.flex(
-            rx.image(src="logo.png",width="30px", 
-            height="30px",border_radius="50%"),
-            rx.text.strong("olbapdev",color=Colors.PRIMARY.value,style=Styles.navbar_title_style)
-        ),
+        rx.link(
+            rx.flex(
+                rx.image(src="logo.png",width="30px", 
+                height="30px",border_radius="50%"),
+                rx.text.strong("olbapdev",color=Colors.PRIMARY.value,style=Styles.navbar_title_style)
+            ),
+        href=routes.Route.INDEX.value),
         position="sticky",
         bg=Colors.CONTENT.value,
         padding_x=Size.DEFAULT.value,

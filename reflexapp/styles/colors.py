02d300b757e0b7ff14699361eb2764b38b1a1150
PJMonticoli <pablo_monticoli@hotmail.com>
2024-07-01 15:25:05 -0300

    agrego nuevo background color a la web

--- reflexapp/styles/colors.py
+++ reflexapp/styles/colors.py
@@ -1,12 +1,17 @@
 from enum import Enum
+
+
 class Colors(Enum):
     PRIMARY = "#14A1F0"
     SECONDARY = "#3b2e7d"
-    BACKGROUND = "#091016" #0C151D color alternativo
+    BACKGROUND = "radial-gradient(125% 125% at 50% 10%, #000 40%, #63e 100%)"
     CONTENT = "#171F26"
-    
+    DESCRIPTION = "#fef08a"
+
+
 class TextColor(Enum):
     HEADER = "#F1F2F4"
     BODY = "#C3C7CB"
     FOOTER = "#A3ABB2"
     WHITE = "#FFFFFF"
+    DESCRIPTION = "#fef08a"
--- reflexapp/styles/styles.py
+++ reflexapp/styles/styles.py
@@ -7,62 +7,64 @@ from .fonts import Font as Font
 MAX_WIDTH = "600px"
 
 # Sizes
+
+
 class Size(Enum):
-    SMALL= "0.5em"
-    MEDIUM= "0.95em"
-    DEFAULT= "1.2em"
+    SMALL = "0.5em"
+    MEDIUM = "0.95em"
+    DEFAULT = "1.2em"
     LARGE = "1.5em"
-    BIG= "2em"
+    BIG = "2em"
 
 # Styles
 
+
 BASE_STYLE = {
-    "font_family" : Font.DEFAULT.value,
-    "background_color" : Color.BACKGROUND.value,
-    rx.button : {
-        "width" : "100%",
-        "height" : "100%",
-        "padding" : Size.SMALL.value,
-        "border_radius" : Size.DEFAULT.value,
-        "color" : TextColor.HEADER.value,
-        "background_color" : Color.CONTENT.value,
-        "cursor" : "pointer", 
-        "white_space":"normal", 
-        "text_align" : "start",
-        "_hover" : {
-          "background_color" : Color.SECONDARY.value,
-          "cursor" : "pointer"  
+    "font_family": Font.DEFAULT.value,
+    "background_image": Color.BACKGROUND.value,
+    rx.button: {
+        "width": "100%",
+        "height": "100%",
+        "padding": Size.SMALL.value,
+        "border_radius": Size.DEFAULT.value,
+        "color": TextColor.HEADER.value,
+        "background_color": Color.CONTENT.value,
+        "cursor": "pointer",
+        "white_space": "normal",
+        "text_align": "start",
+        "_hover": {
+            "background_color": Color.SECONDARY.value,
+            "cursor": "pointer"
         }
     },
     rx.link: {
-        "text_decoration" : "none",
-        "_hover" : {}
+        "text_decoration": "none",
+        "_hover": {}
     }
 }
 
 button_title_style = dict(
-    font_size = Size.DEFAULT.value,
-    font_family = Font.TITLE.value
+    font_size=Size.DEFAULT.value,
+    font_family=Font.TITLE.value
 )
 navbar_title_style = dict(
     font_family=Font.LOGO.value,
-    font_size = Size.DEFAULT.value
+    font_size=Size.DEFAULT.value
 )
 
 button_body_style = dict(
-    font_size = Size.MEDIUM.value,
-    font_family = Font.DEFAULT.value,
+    font_size=Size.MEDIUM.value,
+    font_family=Font.DEFAULT.value,
 )
 
 title_styles = dict(
     size="lg",
-    width ="100%",
-    font_family = Font.DEFAULT.value,
-    padding_top = Size.DEFAULT.value,
-    color= TextColor.WHITE.value,
+    width="100%",
+    font_family=Font.DEFAULT.value,
+    padding_top=Size.DEFAULT.value,
+    color=TextColor.WHITE.value,
 )
 
 STYLESSHEETS = [
-    "https://fonts.googleapis.com/css?family=Poppins:wght@300;500&display=swap",
-    "https://fonts.googleapis.com/css?family=Comfortaa:wght@500&display=swap"
+    "https://fonts.googleapis.com/css2?family=Onest:wght@100..900&display=swap"
 ]
